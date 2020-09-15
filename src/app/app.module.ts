8fb456ea4cd27563d6e4d75f1e0ff9927e57fe48
Nicolas <nic@pop-os.localdomain>
2020-09-15 15:57:07 -0400

    Changed sound-btn ui and added top-bar component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { SoundListComponent } from './sound-list/sound-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
-    SoundListComponent
+    SoundListComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule
